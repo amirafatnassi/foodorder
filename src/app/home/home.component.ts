6a4a7514da2daa383e8cb549a2903cf224901479
amirafatnassi <amirafatnassi88@gmail.com>
2022-08-06 22:53:34 +0100

    add food model update food service getall()

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
+import { Foods } from '../shared/models/food';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,7 @@ import { FoodService } from '../services/food/food.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  foods:String[]=[];
+  foods:Foods[]=[];
 
   constructor(private fs:FoodService) { 
   }
--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,20 +1,47 @@
 import { Injectable } from '@angular/core';
+import { Foods } from 'src/app/shared/models/food';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FoodService {
+  constructor() {}
 
-  constructor() { }
-
-  getAll():string[]{
+  getAll() : Foods[]{
     return [
-      '/assets/food-1.jpg',
-      '/assets/food-2.jpg',
-      '/assets/food-3.jpg',
-      '/assets/food-4.jpg',
-      '/assets/food-5.jpg',
-      '/assets/food-6.jpg',
-    ]
+      {
+        id: 1,
+        name: 'veggies awith salmon and avocado',
+        price: 20,
+        cookTime: '20-30',
+        favorite: true,
+        origins: ['persia', 'middle east', 'china'],
+        star:4,
+        imageUrl: '/assets/images/foods/food-1.jpg',
+        tags: ['dlowFood', 'lunch'],
+      },
+      {
+        id: 2,
+        name: 'vtuna sandwich',
+        price: 20,
+        cookTime: '20-30',
+        favorite: true,
+        origins: ['france', 'japan', 'phillippens'],
+        star:3.9,
+        imageUrl: '/assets/images/foods/food-2.jpg',
+        tags: ['sandwich', 'fastfood'],
+      },
+      {
+        id: 3,
+        name: 'sald with meatballs',
+        price: 20,
+        cookTime: '20-30',
+        favorite: true,
+        origins: ['mexico', 'middle east', 'china'],
+        star:4.2,
+        imageUrl: '/assets/images/foods/food-3.jpg',
+        tags: ['salad', 'healthy'],
+      },
+    ];
   }
 }
