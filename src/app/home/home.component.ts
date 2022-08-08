32944b5943eb7a1026a979a325975b15f9c5e467
amirafatnassi <amirafatnassi88@gmail.com>
2022-08-08 21:19:22 +0100

    add search component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { Foods } from '../shared/models/food';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,11 +11,17 @@ import { Foods } from '../shared/models/food';
 export class HomeComponent implements OnInit {
   foods:Foods[]=[];
 
-  constructor(private fs:FoodService) { 
+  constructor(private fs:FoodService, private router:ActivatedRoute) { 
   }
 
   ngOnInit(): void {
+    this.router.params.subscribe(params=>{
+      if(params['searchItem'])
+      this.foods=this.fs.getAll().filter(food=>food.name.toLowerCase().includes(params['searchItem'].toLowerCase()));
+    else 
     this.foods=this.fs.getAll();
+    })
+    
   }
 
 }
