567200da04067a3ce87de7227ba605c9ce3c5128
amirafatnassi <amirafatnassi88@gmail.com>
2022-08-28 00:31:38 +0100

    add to cart

--- src/app/foodpage/foodpage.component.ts
+++ src/app/foodpage/foodpage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from '../services/cart/cart.service';
 import { FoodService } from '../services/food/food.service';
 import { Foods } from '../shared/models/food';
 
@@ -12,7 +13,9 @@ export class FoodpageComponent implements OnInit {
   food!: Foods;
   constructor(
     private activatedRoute: ActivatedRoute,
-    private foodService: FoodService
+    private foodService: FoodService,
+    private cartService: CartService,
+    private router:Router
   ) {
     activatedRoute.params.subscribe((params)=>{
       if (params['id'])
@@ -21,4 +24,8 @@ export class FoodpageComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+  addToCart(){
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
+  }
 }
